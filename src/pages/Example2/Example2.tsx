0b421f64cc9a1cc5b73624e9aeefe7da4a550fbd
matveygrevtsov <matveygrevcov@gmail.com>
2023-11-06 23:19:59 +0300

    add questions

--- src/pages/Example2/Example2.tsx
+++ src/pages/Example2/Example2.tsx
@@ -1,29 +1,16 @@
-import s from "./style.module.css";
-
 export const Example2 = () => {
   let state: number = 0;
 
-  const handleClickMinus = () => {
-    --state;
-    console.log(state);
-  };
-
-  const handleClickPlus = () => {
+  const handleClick = () => {
     ++state;
     console.log(state);
   };
 
   return (
-    <div className={s.root}>
+    <>
       <div>{state}</div>
-      <div>
-        <button className={s.button} onClick={handleClickMinus}>
-          -
-        </button>
-        <button className={s.button} onClick={handleClickPlus}>
-          +
-        </button>
-      </div>
-    </div>
+      <button onClick={handleClick}>+</button>
+    </>
   );
 };
+
